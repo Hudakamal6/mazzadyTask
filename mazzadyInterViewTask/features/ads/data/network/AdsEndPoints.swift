a1f9b3a9a3cd9dedb4712a0c83fe0acac96f6f31
hudakamal6 <hudakamal9215@gmail.com>
2025-04-29 02:58:49 +0300

    add adsEndPoints

---
+++ mazzadyInterViewTask/features/ads/data/network/AdsEndPoints.swift
@@ -0,0 +1,11 @@
+//
+//  userEndPoints.swift
+//  mazzadyInterViewTask
+//
+//  Created by yearex on 26/04/2025.
+//
+
+import Foundation
+enum AdsEndPoints {
+    static let getAds = "https://stagingapi.mazaady.com/api/interview-tasks/advertisements"
+}
